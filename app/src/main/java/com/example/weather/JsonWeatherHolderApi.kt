492c2229d62fa26b3f4e9758237e94058163d8a5
Filip <fifigaw98@gmail.com>
2020-03-17 00:39:48 +0100

    add lang to get city by name

--- app/src/main/java/com/example/weather/JsonWeatherHolderApi.kt
+++ app/src/main/java/com/example/weather/JsonWeatherHolderApi.kt
@@ -6,7 +6,11 @@ import retrofit2.http.Query
 
 interface JsonWeatherHolderApi {
     @GET("data/2.5/weather")
-    fun getCity(@Query("q") city: String, @Query("APPID") apiKey: String, @Query("units") units: String = "metric"): Call<Json>
+    fun getCity(
+        @Query("q") city: String,
+        @Query("APPID") apiKey: String,
+        @Query("units") units: String = "metric",
+        @Query("lang") lang: String = "pl"): Call<Json>
     @GET("data/2.5/weather")
     fun getCityByID(
         @Query("id") id: Int,
