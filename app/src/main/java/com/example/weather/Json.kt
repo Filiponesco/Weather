8386ffff3e1df0994bcf4d230f814e10d7d4e3f6
Filip <fifigaw98@gmail.com>
2020-03-17 00:36:28 +0100

    add progressBar, lock rotation, change time of city, load Json - coroutines,

--- app/src/main/java/com/example/weather/Json.kt
+++ app/src/main/java/com/example/weather/Json.kt
@@ -1,7 +1,7 @@
 package com.example.weather
 
-data class Json(val weather: List<Weather>, val main: Main, val dt: Long, val sys: Sun) {
+data class Json(val weather: List<Weather>, val main: Main, val dt: Long, val sys: Sun, val name: String, val timezone: Long) {
     data class Weather(val main: String, val description: String, val icon: String)
     data class Main(val temp: Float, val feels_like: Float, val pressure: String, val humidity: String)
-    data class Sun(val sunrise: Long, val sunset: Long)
+    data class Sun(val sunrise: Long, val sunset: Long, val country: String)
 }
--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -26,76 +26,35 @@ import androidx.core.app.ComponentActivity.ExtraData
 import androidx.core.content.ContextCompat.getSystemService
 import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.provider.Settings
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
+import android.view.View
+import kotlinx.coroutines.*
 import kotlin.concurrent.thread
 
-class MainActivity : AppCompatActivity(), CallbackJson {
-    private lateinit var cities: List<City>
-
-    override fun onResponse(list: List<City>) {
-        //Log.e("responseJSONFile", list[0].toString())
-        cities = list
-        //floating_search_view.hideProgress()
-    }
-
+class MainActivity : AppCompatActivity() {
     lateinit var jsonWeatherHolderApi: JsonWeatherHolderApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        changeVisibleUI(false)
+        progressBarLoadingContent.visibility = View.VISIBLE
 
-        val retrofit = Retrofit.Builder()
-            .baseUrl(URLOpenWeather)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+        buildRetrofit()
 
-        jsonWeatherHolderApi = retrofit.create(
-            JsonWeatherHolderApi::class.java
-        )
-        floating_search_view.showProgress()
-        GlobalScope.launch {
-            cities = getCitiesSuspend()
+        getCityInfoByID(IDCityOnStart) //data city on start APP
+
+        floating_search_view.showProgress() //inform user: loading json file with city ID
+        GlobalScope.launch(Dispatchers.Main){
+            val cities = getAllCities() //async
             floating_search_view.hideProgress()
+            setOnQueryChangeListenerSearchBox(cities)
+            setOnClickSuggestionSearchBox()
         }
-//        thread {
-//            getAllCities(this)
-//        }
-
-
-        floating_search_view.setOnQueryChangeListener(FloatingSearchView.OnQueryChangeListener { oldQuery, newQuery ->
-            //get suggestions based on newQuery
-            if(newQuery.isNotEmpty()) {
-                val matchCities = cities.filter { it.name.contains(newQuery) }.take(numberOfPrompt)
-                //pass them on to the search view
-                floating_search_view.swapSuggestions(matchCities)
-            }
-            else{
-                floating_search_view.clearSuggestions()
-            }
-        })
-
-        floating_search_view.setOnSearchListener(object : FloatingSearchView.OnSearchListener {
-            override fun onSuggestionClicked(searchSuggestion: SearchSuggestion) {
-                val mLastCity: City = searchSuggestion as City
-                Log.e("OnSuggestionClicked", mLastCity.name)
-                getCityInfoByID(mLastCity.id)
-                floating_search_view.setSearchBarTitle(mLastCity.name)
-                floating_search_view.clearSearchFocus()
-            }
-
-            override fun onSearchAction(query: String) {
-                val mLastQuery = query
-                Log.e("OnSearchAction", mLastQuery)
-                floating_search_view.clearSearchFocus()
-            }
-        })
     }
     private fun getCityInfo(city: String){
         val call: Call<Json> = jsonWeatherHolderApi.getCity(city, API_KEY)
         call.enqueue(object: Callback<Json>{
             override fun onFailure(call: Call<Json>, t: Throwable) {
-                Toast.makeText(this@MainActivity, messageFailureApi, Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@MainActivity, messageFailureApi, Toast.LENGTH_LONG).show()
                 Log.e("responseApiFailure", t.message)
             }
 
@@ -122,6 +81,8 @@ class MainActivity : AppCompatActivity(), CallbackJson {
                     return
                 }
                 setUI(response.body())
+                progressBarLoadingContent.visibility = View.GONE
+                changeVisibleUI(true)
             }
         })
     }
@@ -133,13 +94,14 @@ class MainActivity : AppCompatActivity(), CallbackJson {
             txtViewTemp.text = "0"
             return
         }
-        txtViewDate.text = convertTimeUnixToString(response.dt, "dd-MM HH:mm")
+        txtViewDate.text = convertTimeUnixToString(response.dt + response.timezone - 3600, "dd-MM HH:mm")
         txtViewDescription.text = response.weather[0].description
         txtViewTemp.text = "${response.main.temp.roundToInt()}"
         txtViewSunrise.text = convertTimeUnixToString(response.sys.sunrise, "HH:mm")
         txtViewSunset.text = convertTimeUnixToString(response.sys.sunset, "HH:mm")
         txtViewPressure.text = "${response.main.pressure}$pascal"
         txtViewFeelsLike.text = "${response.main.feels_like.roundToInt()}$celcius"
+        floating_search_view.setSearchBarTitle("${response.name}, ${response.sys.country}")
         Glide.with(this@MainActivity)
             .load(URLIcon + response.weather[0].icon + "@2x.png")
             .fitCenter()
@@ -152,29 +114,73 @@ class MainActivity : AppCompatActivity(), CallbackJson {
         sdf.format(date)
         return sdf.format(date)
     }
-    private fun getAllCities(listener: CallbackJson){
-        val reader = applicationContext.assets.open("city.list.json").bufferedReader()
-        val cities = Gson().fromJson(reader, Array<City>::class.java).toList()
-        Log.e("JSON file", cities[0].toString())
-        listener.onResponse(cities)
+    private suspend fun getAllCities(): List<City>{
+        return GlobalScope.async(Dispatchers.IO) {
+            val reader = applicationContext.assets.open("city.list.json").bufferedReader()
+            val cities = Gson().fromJson(reader, Array<City>::class.java).toList()
+            Log.e("JSON file", cities[0].toString())
+            return@async cities
+        }.await()
     }
-    suspend fun getCitiesSuspend(): List<City>{
-        val reader = applicationContext.assets.open("city.list.json").bufferedReader()
-        val cities = Gson().fromJson(reader, Array<City>::class.java).toList()
-        Log.e("JSON file", cities[0].toString())
-        return cities
+    private fun setOnQueryChangeListenerSearchBox(cities: List<City>){
+        floating_search_view.setOnQueryChangeListener(FloatingSearchView.OnQueryChangeListener { oldQuery, newQuery ->
+            //get suggestions based on newQuery
+            if(newQuery.isNotEmpty()) {
+                val matchCities = cities.filter { it.name.contains(newQuery) }.take(numberOfPrompt)
+                //pass them on to the search view
+                floating_search_view.swapSuggestions(matchCities)
+            }
+            else{
+                floating_search_view.clearSuggestions()
+            }
+        })
+    }
+    private fun setOnClickSuggestionSearchBox(){
+        floating_search_view.setOnSearchListener(object : FloatingSearchView.OnSearchListener {
+            override fun onSuggestionClicked(searchSuggestion: SearchSuggestion) {
+                val mLastCity: City = searchSuggestion as City
+                Log.e("OnSuggestionClicked", mLastCity.toString())
+                getCityInfoByID(mLastCity.id)
+                floating_search_view.clearSearchFocus()
+            }
+            override fun onSearchAction(query: String) {
+                val mLastQuery = query
+                Log.e("OnSearchAction", mLastQuery)
+                floating_search_view.clearSearchFocus()
+            }
+        })
+    }
+    private fun buildRetrofit(){
+        val retrofit = Retrofit.Builder()
+            .baseUrl(URLOpenWeather)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        jsonWeatherHolderApi = retrofit.create(
+            JsonWeatherHolderApi::class.java
+        )
+    }
+    fun changeVisibleUI(visible: Boolean){
+        if(visible){
+            floating_search_view.visibility = View.VISIBLE
+            cardViewExtraData.visibility = View.VISIBLE
+            linearLayoutContentUp.visibility = View.VISIBLE
+        }
+        else{
+            floating_search_view.visibility = View.INVISIBLE
+            cardViewExtraData.visibility = View.INVISIBLE
+            linearLayoutContentUp.visibility = View.INVISIBLE
+        }
     }
     companion object{
         const val URLOpenWeather: String = "http://api.openweathermap.org/"
         const val API_KEY: String = "2c3a4150c327f165dd4e954b18ca56bf"
         const val URLIcon = "http://openweathermap.org/img/wn/"
         const val numberOfPrompt = 3
-        const val messageFailureApi = "Coś poszło nie tak"
+        const val messageFailureApi = "Coś poszło nie tak, włącz internet"
         const val messageResponseNoSuccessful = "Brak odpowiedzi. Kod: "
         const val celcius = "°C"
         const val pascal = "hPa"
+        const val IDCityOnStart = 3086800
     }
-}
-interface CallbackJson{
-    fun onResponse(list: List<City>)
 }
